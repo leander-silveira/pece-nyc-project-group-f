a3c5c5064a81d2530f491e3551de86e30bdb6eea
Bianca Garcia Martins <biigarciam@gmail.com>
2025-03-16 00:47:38 -0300

    fix

--- load_dw_to_rds.py
+++ load_dw_to_rds.py
@@ -1,6 +1,19 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
+import os
+import boto3
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when, trim, current_timestamp, lower, lit
+from pyspark.sql.types import IntegerType, FloatType, StringType, TimestampType
+
+# Definir caminho correto para os JARs no Cloud9
+home_dir = os.environ["HOME"]
+jars_path = f"{home_dir}/spark_jars/hadoop-aws-3.3.1.jar,{home_dir}/spark_jars/aws-java-sdk-bundle-1.11.901.jar"
+
+
+
+
 # Criar sessão do Spark com suporte a S3 e JDBC
 spark = SparkSession.builder \
     .appName("S3 DW to RDS") \
@@ -10,12 +23,12 @@ spark = SparkSession.builder \
     .config("spark.sql.parquet.mergeSchema", "false") \
     .config("spark.sql.parquet.filterPushdown", "true") \
     .config("spark.sql.files.ignoreCorruptFiles", "true") \
-    .config("spark.jars", "/path/to/mysql-connector-java-8.0.33.jar") \
+    .config("spark.jars", jars_path) \
     .getOrCreate()
 
 # Definir caminhos e configurações
-s3_path = "s3://mba-nyc-dataset/dw/taxis/"
-jdbc_url = "jdbc:mysql://database-1.coseekllgrql.us-east-1.rds.amazonaws.com:3306/your_database"
+s3_path = "s3a://mba-nyc-dataset/dw/taxis/"
+jdbc_url = "jdbc:mysql://database-1.coseekllgrql.us-east-1.rds.amazonaws.com:3306/database-1"
 db_table = "taxi_trips"
 db_user = "admin"  # Substitua pelo usuário correto
 db_password = "pecenycgrupof"  # Substitua pela senha correta (não recomendado hardcode)
